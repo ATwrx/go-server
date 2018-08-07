149c16aaae602ba812e294574ad944b323d2a504
A. Thompson <act.webdesigns@gmail.com>
2018-08-06 22:35:46 -0500

    Functioning API

--- main.go
+++ main.go
@@ -2,33 +2,88 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+
+	// _ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Homepage endpoint hit")
-	fmt.Fprintf(w, "Homepage endpoint hit")
+var db *gorm.DB
+var err error
+
+// User model
+type User struct {
+	gorm.Model
+	UserName string `json:"username"`
+	Email    string `json:"email"`
 }
 
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
+func homePage(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"Test": "success"})
+	fmt.Println("Root endpoint hit")
+}
 
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/users", AllUsers).Methods("GET")
-	myRouter.HandleFunc("/user/{name}/{email}", NewUser).Methods("POST")
-	myRouter.HandleFunc("/user/{name}", DeleteUser).Methods("DELETE")
-	myRouter.HandleFunc("/user/{name}/{email}", UpdateUser).Methods("UPDATE")
-	myRouter.HandleFunc("/articles", AllPosts).Methods("GET")
-	myRouter.HandleFunc("/articles", TestPosts).Methods("POST")
+func createUser(c *gin.Context) {
+	var user User     // create a ref User struct in mem
+	c.BindJSON(&user) // bind router context data to User ref as JSON
+	db.Create(&user)  // create User table in db
+	c.JSON(200, user) // send server response
+	return
+}
 
-	log.Fatal(http.ListenAndServe(":8081", myRouter))
+func deleteUser(c *gin.Context) {
+	var user User
+	id := c.Params.ByName("id")
+	db.Where("id = ?", id).Find(&user)
+	db.Delete(user)
+	return
+}
+
+func getAllUsers(c *gin.Context) {
+	var users []User
+	db.Find(&users)
+	c.JSON(http.StatusOK, users)
+	return
+}
+
+func updateUser(c *gin.Context) {
+	username := c.Query("username")
+	email := c.Query("email")
+	user := User{UserName: username, Email: email}
+	db.Where("email = ?", email).Find(&user)
+	user.UserName = username
+	db.Save(user)
+	return
+}
+
+func seedUsers() {
+	u1 := User{UserName: "tim", Email: "t@t.com"}
+	u2 := User{UserName: "tay", Email: "s@t.com"}
+	u3 := User{UserName: "tom", Email: "w@t.com"}
+	db.Create(&u1)
+	db.Create(&u2)
+	db.Create(&u3)
 }
 
 func main() {
-	InitDB()
-	handleRequests()
-	fmt.Println("Go ORM initiated")
+	db, err = gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to database")
+	}
+	defer db.Close()
+	db.AutoMigrate(&User{})
+
+	//seedUsers()           // uncomment to add 3 users as seed data
+	r := gin.Default()
+	r.GET("/", homePage)
+	r.GET("/users", getAllUsers)             // /users Return all users
+	r.POST("/user/new", createUser)          // /user/new Create a new user
+	r.DELETE("/user/delete/:id", deleteUser) // /user/delete/?name Delete a user
+	r.PUT("/user/change/", updateUser)       // Update a users email
+	r.Run()                                  // Listen and serve on :8080
+
 }
